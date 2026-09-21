0085266336f7891e274aa9939b8ecf7bd215f466
TiagoGante <tiagogante@outlook.pt>
2026-09-21 22:12:47 +0100

    feat: add cart page tests

--- tests/fixtures/test.ts
+++ tests/fixtures/test.ts
@@ -2,11 +2,13 @@ import { test as base } from "@playwright/test"
 import { HomePage } from "../page-objects/home.page"
 import { CatalogPage } from "../page-objects/catalog.page"
 import { ProductPage } from "../page-objects/product.page"
+import { CartPage } from "../page-objects/cart.page"
 
 type Fixtures = {
   homePage: HomePage
   catalogPage: CatalogPage
   productPage: ProductPage
+  cartPage: CartPage
 }
 
 export const test = base.extend<Fixtures>({
@@ -18,6 +20,9 @@ export const test = base.extend<Fixtures>({
   },
   productPage: async ({ page }, use) => {
     await use(new ProductPage(page))
+  },
+  cartPage: async ({ page }, use) => {
+    await use(new CartPage(page))
   },
 })
 
